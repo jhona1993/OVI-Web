639c5d6bfa3f3a36d7cc043a650b56857fa62111
JHOANN ADRIAN RAMIREZ CARVAJAL <jhoann.ramirez@apuestascucuta75.co>
2019-04-06 12:19:18 -0500

    Agregaron los videos con su funcion

---
+++ js/index.js
@@ -0,0 +1,41 @@
+$('#video1').click(function(event) {
+    event.preventDefault();
+    var href = $('#video1').attr('href');
+    $("#cont-principal").attr("src", href);
+    console.log(href);
+});
+
+$('#video2').click(function(event) {
+    event.preventDefault();
+    var href = $('#video2').attr('href');
+    $("#cont-principal").attr("src", href);
+    console.log(href);
+});
+
+$('#video3').click(function(event) {
+    event.preventDefault();
+    var href = $('#video3').attr('href');
+    $("#cont-principal").attr("src", href);
+    console.log(href);
+});
+
+$('#video4').click(function(event) {
+    event.preventDefault();
+    var href = $('#video4').attr('href');
+    $("#cont-principal").attr("src", href);
+    console.log(href);
+});
+
+$('#video5').click(function(event) {
+    event.preventDefault();
+    var href = $('#video5').attr('href');
+    $("#cont-principal").attr("src", href);
+    console.log(href);
+});
+
+$('#video6').click(function(event) {
+    event.preventDefault();
+    var href = $('#video6').attr('href');
+    $("#cont-principal").attr("src", href);
+    console.log(href);
+});
