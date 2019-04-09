8e6115344f7330a331b45e2bc58bf1edd308b983
Technologist <jhoann.ramirez.carvajal@gmail.com>
2019-04-09 17:02:57 -0500

    Se modifico la pagina de Multimedia

--- js/index.js
+++ js/index.js
@@ -1,41 +1,42 @@
 $('#video1').click(function(event) {
+    console.log('Hola');
     event.preventDefault();
-    var href = $('#video1').attr('href');
-    $("#cont-principal").attr("src", href);
+    var href = $(this).attr('href');
+    $("#re-video").attr("src", href);
     console.log(href);
 });
 
 $('#video2').click(function(event) {
     event.preventDefault();
     var href = $('#video2').attr('href');
-    $("#cont-principal").attr("src", href);
+    $("#re-video").attr("src", href);
     console.log(href);
 });
 
 $('#video3').click(function(event) {
     event.preventDefault();
     var href = $('#video3').attr('href');
-    $("#cont-principal").attr("src", href);
+    $("#re-video").attr("src", href);
     console.log(href);
 });
 
 $('#video4').click(function(event) {
     event.preventDefault();
     var href = $('#video4').attr('href');
-    $("#cont-principal").attr("src", href);
+    $("#re-video").attr("src", href);
     console.log(href);
 });
 
 $('#video5').click(function(event) {
     event.preventDefault();
     var href = $('#video5').attr('href');
-    $("#cont-principal").attr("src", href);
+    $("#re-video").attr("src", href);
     console.log(href);
 });
 
 $('#video6').click(function(event) {
     event.preventDefault();
     var href = $('#video6').attr('href');
-    $("#cont-principal").attr("src", href);
+    $("#re-video").attr("src", href);
     console.log(href);
 });
